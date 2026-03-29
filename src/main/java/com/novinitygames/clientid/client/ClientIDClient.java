1e782c2c9e18b04ea22c588c85c5e7bae2790eb7
minecraft-ys <131407100+minecraft-ys@users.noreply.github.com>
2026-03-29 15:42:59 -0700

    e

--- src/main/java/com/novinitygames/clientid/client/ClientIDClient.java
+++ src/main/java/com/novinitygames/clientid/client/ClientIDClient.java
@@ -1,22 +1,21 @@
-package com.novinitygames.clientid.client.util;
-
-import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
+package com.novinitygames.clientid.client;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class ListerUtil {
-    public static ArrayList<String> getMods() {
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+public class ClientIDClient {
+
+    public static boolean pieChartDisabled = false;
+    public static boolean isConnectedToServer = false;
+
+    public static String getClientID() {
         Collection<ModContainer> mods = FabricLoader.getInstance().getAllMods();
-        ArrayList<String> list = new ArrayList<>();
-        mods.forEach(mod -> {
-            String id = mod.getMetadata().getId();
-            // Only include "fabric-api" and "clientid"
-            if ("fabric-api".equals(id) || "clientid".equals(id)) {
-                list.add(id);
-            }
-        });
-        return list;
+        ArrayList<String> modIDs = new ArrayList<>();
+        for (ModContainer mod : mods) {
+            modIDs.add(mod.getMetadata().getId());
+        }
+        return String.join(",", modIDs);
     }
 }
