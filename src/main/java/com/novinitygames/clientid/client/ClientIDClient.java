ce65508d7fd00420b98ac77535d227a9c76c5fab
Yurii Savchuk <131407100+minecraft-ys@users.noreply.github.com>
2026-03-29 15:00:20 -0700

    excludes all exept fabricapi and clientid

--- src/main/java/com/novinitygames/clientid/client/ClientIDClient.java
+++ src/main/java/com/novinitygames/clientid/client/ClientIDClient.java
@@ -1,61 +1,22 @@
-package com.novinitygames.clientid.client;
+package com.novinitygames.clientid.client.util;
 
-import com.novinitygames.clientid.client.records.*;
-import com.novinitygames.clientid.client.util.ListerUtil;
-import com.novinitygames.clientid.client.watcher.ResourcePackWatcher;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.loader.api.ModContainer;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
 
-public class ClientIDClient implements ClientModInitializer {
-    ResourcePackWatcher watcher;
-
-    public static Boolean isConnectedToServer = false;
-    public static Boolean pieChartDisabled = false;
-
-    @Override
-    public void onInitializeClient() {
-        watcher = new ResourcePackWatcher();
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            isConnectedToServer = true;
-
-            // Initial installation confirmation
-            ModCheckC2SPayload payload = new ModCheckC2SPayload(MinecraftClient.getInstance().getGameProfile().id().toString());
-            ClientPlayNetworking.send(payload);
-
-            // Mods
-            List<String> installedModsList = ListerUtil.getMods();
-            String installedMods = String.join(",", installedModsList);
-
-            ModListC2SPayload payload2 = new ModListC2SPayload(installedMods);
-            ClientPlayNetworking.send(payload2);
-
-            // Resource packs
-            List<String> enabledPacksList = ListerUtil.getEnabledPacks();
-            String enabledPacks = String.join(",", enabledPacksList);
-
-            PackListC2SPayload payload3 = new PackListC2SPayload(enabledPacks);
-            ClientPlayNetworking.send(payload3);
-
-            // Version
-            FabricLoader.getInstance().getModContainer("clientid").ifPresent(modContainer -> {
-                String version = modContainer.getMetadata().getVersion().getFriendlyString();
-                VersionC2SPayload payload4 = new VersionC2SPayload(version);
-                ClientPlayNetworking.send(payload4);
-            });
+public class ListerUtil {
+    public static ArrayList<String> getMods() {
+        Collection<ModContainer> mods = FabricLoader.getInstance().getAllMods();
+        ArrayList<String> list = new ArrayList<>();
+        mods.forEach(mod -> {
+            String id = mod.getMetadata().getId();
+            // Only include "fabric-api" and "clientid"
+            if ("fabric-api".equals(id) || "clientid".equals(id)) {
+                list.add(id);
+            }
         });
-
-        ClientPlayConnectionEvents.DISCONNECT.register((handler, sender) -> {
-            isConnectedToServer = false;
-            pieChartDisabled = false;
-        });
-
-        ClientPlayNetworking.registerGlobalReceiver(ChartsS2CPayload.ID, (payload, ctx) -> {
-            pieChartDisabled = payload.val();
-        });
+        return list;
     }
 }
